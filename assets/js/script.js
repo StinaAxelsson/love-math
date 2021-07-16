879478035ce60a5500f239f0aa6f28fa75956ed6
StinaAxelsson <stina91@live.se>
2021-07-16 13:38:52 +0000

    Add code to increment score and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     runGame("addition");
-})
+});
 
 /**
  * The main game "loop", called when the script is first loaded
@@ -37,19 +37,22 @@ function runGame(gameType){
  * Checks the answer against the first element in the returned
  * calculateCorrectAnswer array
  */
-function checkAnswer(){
+ function checkAnswer() {
 
     let userAnswer = parseInt(document.getElementById("answer-box").value);
     let calculatedAnswer = calculateCorrectAnswer();
     let isCorrect = userAnswer === calculatedAnswer[0];
 
     if (isCorrect) {
-        alert("Hey You got it right! :D");
+        alert("Hey! You got it right! :D");
+        incrementScore();
     } else {
-        alert(`Awww..... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
+
 }
 
 /**
@@ -71,12 +74,22 @@ function calculateCorrectAnswer(){
 
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 function incrementScore(){
 
+     let oldScore = parseInt(document.getElementById("score").innerText);
+     document.getElementById("score").innerText = ++oldScore;
 }
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
 function incrementWrongAnswer(){
 
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2){
